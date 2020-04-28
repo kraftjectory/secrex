50b3aba2d0af1949dffa4a15d2a95c3a38cebcd8
Cẩm Huỳnh <huynhquancam@gmail.com>
2020-04-28 14:18:51 +0200

    Refactor cipher modules

--- lib/mix/tasks/secrex.decrypt.ex
+++ lib/mix/tasks/secrex.decrypt.ex
@@ -15,11 +15,16 @@ defmodule Mix.Tasks.Secrex.Decrypt do
 
     for path <- secret_files() do
       enc_path = encrypted_path(path)
-      Mix.shell().info("decrypting #{enc_path}")
-      decrypted = decrypt(enc_path, key)
-      File.write!(path, decrypted)
-    end
+      Mix.shell().info("Decrypting #{enc_path}")
 
-    Mix.shell().info("files decrypted")
+      case decrypt(enc_path, key) do
+        {:ok, decrypted} ->
+          File.write!(path, decrypted)
+          Mix.shell().info("Files have been decrypted")
+
+        {:error, reason} ->
+          Mix.raise("Could not decrypt file, reason: " <> inspect(reason))
+      end
+    end
   end
 end
--- lib/mix/tasks/secrex.encrypt.ex
+++ lib/mix/tasks/secrex.encrypt.ex
@@ -14,11 +14,13 @@ defmodule Mix.Tasks.Secrex.Encrypt do
     key = encryption_key()
 
     for path <- secret_files() do
-      Mix.shell().info("encrypting #{path}")
-      encrypted = encrypt(path, key)
+      Mix.shell().info("Encrypting #{path}")
+
+      {:ok, encrypted} = encrypt(path, key)
+
       File.write!(encrypted_path(path), encrypted)
     end
 
-    Mix.shell().info("files encrypted")
+    Mix.shell().info("Files encrypted")
   end
 end
