d8f7ce3e844a5521dd94502b64de928f3eba6eb7
Aleksei Magusev <lexmag@me.com>
2022-10-14 22:25:08 -0700

    Improve Secrex.AES tests

--- test/secrex/aes_test.exs
+++ test/secrex/aes_test.exs
@@ -5,20 +5,33 @@ defmodule Secrex.AESTest do
 
   import ExUnitProperties
   import StreamData
+  import Bitwise
 
   doctest Secrex.AES
 
   property "encrypt/2 and decrypt/2" do
     check all(
+            key <- binary(min_length: 1),
             plaintext <- binary(min_length: 1),
-            key <- binary(min_length: 1)
+            aad <- binary()
           ) do
-      assert {:ok, ciphertext} = AES.encrypt(plaintext, key)
+      assert {:ok, ciphertext} = AES.encrypt(plaintext, key, aad)
 
-      assert AES.decrypt(ciphertext, key) == {:ok, plaintext}
+      assert AES.decrypt(ciphertext, key, aad) == {:ok, plaintext}
 
-      incorrect_key = key <> "A"
-      assert AES.decrypt(ciphertext, incorrect_key) == {:error, :incorrect_key_or_ciphertext}
+      incorrect_key = corrupt_binary(key)
+      assert AES.decrypt(ciphertext, incorrect_key, aad) == {:error, :incorrect_key_or_ciphertext}
+
+      incorrect_aad = corrupt_binary(aad)
+      assert AES.decrypt(ciphertext, key, incorrect_aad) == {:error, :incorrect_key_or_ciphertext}
     end
+  end
+
+  defp corrupt_binary(value) do
+    bit_position = :rand.uniform(bit_size(value) + 1) - 1
+
+    bsl(1, bit_position)
+    |> bxor(:binary.decode_unsigned(value))
+    |> :binary.encode_unsigned()
   end
 end
